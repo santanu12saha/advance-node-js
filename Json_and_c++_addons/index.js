9ffa9e1af4fe6641527a21e0697255e175097e69
santanu12saha <saha.santanu0217@gmail.com>
2020-07-26 12:12:22 +0530

    Add Json and C++ Addons compile for node js

---
+++ Json_and_c++_addons/index.js
@@ -0,0 +1,11 @@
+//require('something');
+
+// 1. try something.js
+// 2. try something.json
+// 3. try something.node
+
+const data = require('data.json');
+console.log(data);
+
+const config = require('./config.json');
+console.log(config);
