d7b229074dd4960bf96a2ac5d51d61ed38ed33a2
santanu12saha <saha.santanu0217@gmail.com>
2020-08-02 19:53:08 +0530

    add time and name for user.

--- NodeForNetworking/MultipleSocket/server.js
+++ NodeForNetworking/MultipleSocket/server.js
@@ -2,23 +2,35 @@ const server = require('net').createServer();
 let counter = 0;
 let sockets = {};
 
+function timestamp () {
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}`;    
+}
+
 server.on('connection', socket => {
     socket.id = counter++;
-    sockets[socket.id] = socket;
-    console.log('Client connected!\n');
+
+    console.log('Client connected');
+    socket.write('Please type your name: ');
+
     socket.on('data', data => {
-        Object.keys(sockets).map(Number).forEach((k) => {
-            if(k !== socket.id) {
-                let cs = sockets[k];
-                cs.write(`${socket.id}:`);
-                cs.write(data);
-            }
+        if(!sockets[socket.id]) {
+            socket.name = data.toString().trim();
+            socket.write(`Welcome ${socket.name}!\n`);
+            sockets[socket.id] = socket;
+            return;
+        }
+
+        Object.entries(sockets).forEach(([key, cs]) => {
+            if(socket.id == key) return;
+            cs.write(`${socket.name} ${timestamp()}: `);
+            cs.write(data);
         });
     });
 
     socket.on('end', () => {
         delete sockets[socket.id];
-        console.log('Client disconnected!\n');
+        console.log('Client disconnected!');
     });
 });
 
